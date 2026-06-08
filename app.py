81d0e86ee7dd726f4bc18e302288727d7476d201
sAkhil2027 <sakhilvikram@gmail.com>
2026-06-08 18:30:34 +0530

    Change GROQ_API_KEY source to Streamlit secrets

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import os
 # ===============================
 load_dotenv()
 
-GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+GROQ_API_KEY = st.secrets["GROQ_API_KEY"]
 
 # ===============================
 # Groq Client
